a1434d1dba74900e07452a1f2bdcfda20f55a093
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-08-09 14:32:06 -0400

    Update

--- challenge/task.cpp
+++ challenge/task.cpp
@@ -21,6 +21,17 @@ Details:
 Example:
 execute(job, 10);
 */
+void execute(std::function<void(int)> t, int load)
+{
+    std::packaged_task<void(int)> task(t);
+    std::future<void> future = task.get_future();
+    std::thread worker(std::move(task), load);
+    worker.join();
+    future.get();
+    std::cout << "Task completed." << '\n';
+}
+
+
 
 int main()
 {
